f1d2bcbfaa2738ccfbe4a38ef5e6afd7b8434f9f
devill-i <imranchopdar02@gmail.com>
2023-05-06 13:50:53 +0530

    version 1.1.1 ,more to be updates in future

--- lib/screens/mainScreen.dart
+++ lib/screens/mainScreen.dart
@@ -16,23 +16,26 @@ class _mainScreenState extends State<mainScreen> {
 
   List<NewsModel>  newsModelList = <NewsModel>[];
   List<String> navBarItems = [
-    "Top news" ,"india","udaipur","ctae","apple","banana",
-    "hindustan","bharat","yash","hello","how","are  ","you ","guys"
+    "Top news" ,"india","udaipur","ctae","vegetables","potlitics",
+    "hindustan","bharat","fruits",
   ];
 bool isLoading = true;
   getNewsByQuery() async{
-    Response response= await get(Uri.parse("https://newsapi.org/v2/everything?q=world&from=2023-03-05&sortBy=publishedAt&apiKey=fa5b68512b554d3e944fc2d475bd9c55"));
-   Map data = jsonDecode(response.body);
-  setState(() {
-    data["articles"].forEach((element){
-      NewsModel newsmodel = new NewsModel();
-      newsmodel= NewsModel.fromMap(element);
-      newsModelList.add(newsmodel);
-      setState(() {
-        isLoading = false;
-      });
+    Response response= await get(Uri.parse("https://newsapi.org/v2/everything?q=world&from=2023-05-04&sortBy=publishedAt&apiKey=fa5b68512b554d3e944fc2d475bd9c55"));
+      Map data = jsonDecode(response.body);
+      print(data);
+setState(() {
+  data["articles"].forEach((element){
+    NewsModel newsmodel = new NewsModel();
+    newsmodel= NewsModel.fromMap(element);
+    newsModelList.add(newsmodel);
+    setState(() {
+
     });
   });
+});
+
+
 
   }
 
@@ -81,7 +84,7 @@ bool isLoading = true;
                       ),
                     ),
                   ),
-                  Container(
+                  SizedBox(
                     height: 50,
                     width: double.infinity,
                     child: ListView.builder(
@@ -91,7 +94,7 @@ bool isLoading = true;
                         itemBuilder: (context, index){
                           return Container(
                            // padding: EdgeInsets.only(left: 10),
-                            margin: EdgeInsets.only(left: 10),
+                            margin: const EdgeInsets.only(left: 10),
                             width: 100,
                             height: 40,
                             decoration: BoxDecoration(
@@ -107,35 +110,33 @@ bool isLoading = true;
                   //SlideShow
                     CarouselSlider(items:newsModelList.map((instance) {
                       return Builder(builder: (context) {
-                        return Container(
-                          child: Card(
-                            shape: RoundedRectangleBorder(
-                              side: BorderSide(color: Colors.white70, width: 1),
-                              borderRadius: BorderRadius.circular(10),
-                            ),
+                        return Card(
+                          shape: RoundedRectangleBorder(
+                            side: const BorderSide(color: Colors.white70, width: 1),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
                          //   margin: EdgeInsets.all(20.0),
-                            child: Stack(
-                              children: [
-                                ClipRRect(
-                                  child: Image.network(instance.newsImg,fit: BoxFit.fill,),
-                                ),
-                                Positioned(
-                                    left: 0,
-                                    bottom:0,
-                                    right:0,
-                                    child: Container(
-                                        height : 40,
-                                        padding: EdgeInsets.all(5.0),
-                                        decoration: BoxDecoration(
-                                          color: Colors.white70,
-                                          borderRadius: BorderRadius.only(bottomLeft:Radius.circular(10),bottomRight: Radius.circular(10)),
-                                        ) ,
-                                        child: Text(instance.newsHead , style: TextStyle(fontWeight: FontWeight.w700,fontSize: 15),)))
-                              ],
-
-                            ),
+                          child: Stack(
+                            children: [
+                              ClipRRect(
+                                child: Image.network(instance.newsImg,fit: BoxFit.fill,),
+                              ),
+                              Positioned(
+                                  left: 0,
+                                  bottom:0,
+                                  right:0,
+                                  child: Container(
+                                      height : 40,
+                                      padding: EdgeInsets.all(5.0),
+                                      decoration: BoxDecoration(
+                                        color: Colors.white70,
+                                        borderRadius: BorderRadius.only(bottomLeft:Radius.circular(10),bottomRight: Radius.circular(10)),
+                                      ) ,
+                                      child: Text(instance.newsHead , style: TextStyle(fontWeight: FontWeight.w700,fontSize: 15),)))
+                            ],
 
                           ),
+
                         );
                       }
                       );
